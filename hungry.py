5d69cf1193350ed8d50b5e40feca093e4c7423f6
yashbehare <yashbehare14@gmail.com>
2023-01-24 19:48:33 +0530

    CR2: conflicts resolved in main and CR2

--- hungry.py
+++ hungry.py
@@ -1,14 +1,14 @@
 
-hungry = input("Are you hungry ?")
+hungry = input("Are you hungry now ?")
 
 if hungry == "yes" and hungry=="yo":
-    print("Eatsamosa")
-    print("Eatpizza")
-    print("Eatburger")
-    print("Eatfries")
-    print("Eathotdog")
+    print("Eat samosa")
+    print("Eat pizza")
+    print("Eat burger")
+    print("Eat fries")
+    print("Eat hotdog")
 else:
     thirsty = input("Are you thirsty")
     if thirsty == "yes":
-        print("Drinkwater")
-        print("Drinksoda")
+        print("Drink water")
+        print("Drink soda")

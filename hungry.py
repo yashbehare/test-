6e5beeb1cdfe1d63dcbb145812aac7582d9a8dc2
yashbehare <yashbehare14@gmail.com>
2023-01-24 19:49:54 +0530

    Main: added new branch

--- hungry.py
+++ hungry.py
@@ -2,13 +2,13 @@
 hungry = input("Are you hungry ?")
 
 if hungry == "yes" and hungry=="yo":
-    print("Eat samosa")
-    print("Eat pizza")
-    print("Eat burger")
-    print("Eat fries")
-    print("Eat hotdog")
+    print("Eatsamosa")
+    print("Eatpizza")
+    print("Eatburger")
+    print("Eatfries")
+    print("Eathotdog")
 else:
     thirsty = input("Are you thirsty")
     if thirsty == "yes":
-        print("Drink water")
-        print("Drink soda")
+        print("Drinkwater")
+        print("Drinksoda")
